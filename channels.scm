97c610ccf5dd289243d431ebe281de454d344616
Michael Zappa <michael@pop-os.localdomain>
2021-09-30 20:33:50 -0400

    flatwhatson's channel for emacs nativecomp/pgtk

--- channels.scm
+++ channels.scm
@@ -1,4 +1,7 @@
 (cons* (channel
         (name 'mz-guix)
         (url "https://github.com/michzappa/guixpkgs.git"))
+       (channel
+        (name 'flatwhatson)
+        (url "https://github.com/flatwhatson/guix-channel.git"))
        %default-channels)
