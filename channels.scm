f7f3a793a150c693f473aa7c8b086166a1ba0434
Michael Zappa <me@michzappa.com>
2021-11-22 16:56:04 -0500

    make remote channel default

--- channels.scm
+++ channels.scm
@@ -1,8 +1,8 @@
 (cons*
  (channel
   (name 'mz-guix)
-  ;; (url "https://github.com/michzappa/guixpkgs.git"))
-  (url "file:///home/michael/projects/guixpkgs"))
+  (url "https://github.com/michzappa/guixpkgs.git"))
+  ;; (url "file:///home/michael/projects/guixpkgs"))
  (channel
   (name 'flatwhatson)
   (url "https://github.com/flatwhatson/guix-channel.git"))
