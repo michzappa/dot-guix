49b342bc4a9e89c9ea2e35557d63a9684fc5f333
Michael Zappa <me@michzappa.com>
2021-12-10 20:39:04 -0500

    channels.scm update

--- channels.scm
+++ channels.scm
@@ -1,19 +1,4 @@
 (list (channel
-        (name 'mz-guix)
-        (url "file:///home/michael/.config/guix")
-        (commit
-          "cce9a5b9f6a41d7ff50f7acb5a4b5a66013370a7"))
-      (channel
-        (name 'flat)
-        (url "https://github.com/flatwhatson/guix-channel.git")
-        (commit
-          "565cc48edc6c547833aa64df7eca6dc04ceb55cb"))
-      (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (commit
-          "5b1184a103640186fd22e1c53ba64a83f7e6fc3d"))
-      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
@@ -22,4 +7,19 @@
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (commit
+          "5b1184a103640186fd22e1c53ba64a83f7e6fc3d"))
+      (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (commit
+          "565cc48edc6c547833aa64df7eca6dc04ceb55cb"))
+      (channel
+        (name 'mz-guix)
+        (url "https://github.com/michzappa/guix.git")
+        (commit
+          "68051e1c75a6160eabd2f850a206a5fc39a0ae2b")))
